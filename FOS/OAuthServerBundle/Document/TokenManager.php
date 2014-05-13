a7cc3eee2c94f501188c18efa67e1c014fec08ad
Asier <uthopiko@gmail.com>
2014-05-13 10:48:36 +0200

    First commit

---
+++ FOS/OAuthServerBundle/Document/TokenManager.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the FOSOAuthServerBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\OAuthServerBundle\Document;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use FOS\OAuthServerBundle\Model\TokenInterface;
+use FOS\OAuthServerBundle\Model\TokenManager as BaseTokenManager;
+
+class TokenManager extends BaseTokenManager
+{
+    protected $dm;
+
+    protected $repository;
+
+    protected $class;
+
+    public function __construct(DocumentManager $dm, $class)
+    {
+        $this->dm = $dm;
+        $this->repository = $dm->getRepository($class);
+        $this->class = $class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findTokenBy(array $criteria)
+    {
+        return $this->repository->findOneBy($criteria);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateToken(TokenInterface $token)
+    {
+        $this->dm->persist($token);
+        $this->dm->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteToken(TokenInterface $token)
+    {
+        $this->dm->remove($token);
+        $this->dm->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteExpired()
+    {
+        return $this
+            ->repository
+            ->createQueryBuilder()
+            ->findAndRemove()
+            ->field('expiresAt')->lt(time())
+            ->getQuery()
+            ->execute();
+    }
+}
---
+++ FOS/OAuthServerBundle/FOSOAuthServerBundle.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the FOSOAuthServerBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\OAuthServerBundle;
+
+use FOS\OAuthServerBundle\DependencyInjection\FOSOAuthServerExtension;
+use FOS\OAuthServerBundle\DependencyInjection\Security\Factory\OAuthFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
+
+class FOSOAuthServerBundle extends Bundle
+{
+    public function __construct()
+    {
+        $this->extension = new FOSOAuthServerExtension();
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        if (version_compare(Kernel::VERSION, '2.1.0-DEV', '>=')) {
+            $extension->addSecurityListenerFactory(new OAuthFactory);
+        }
+    }
+}
